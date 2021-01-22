53a0d9ac128dee5c8ba825ccb7b39b27a14eb6de
Courtney Newcomer <courtney.j.newcomer@gmail.com>
2021-01-21 23:11:02 -0600

    finished and tested signup route:

--- backend/routes/api/session.js
+++ backend/routes/api/session.js
@@ -29,4 +29,13 @@ router.post(
   }),
 );
 
+// Log out
+router.delete(
+  '/',
+  (_req, res) => {
+    res.clearCookie('token');
+    return res.json({ message: 'success' });
+  }
+);
+
 module.exports = router;
--- backend/routes/api/users.js
+++ backend/routes/api/users.js
@@ -1,4 +1,23 @@
-const express = require('express')
+const express = require('express');
+const asyncHandler = require('express-async-handler');
+
+const { setTokenCookie, requireAuth } = require('../../utils/auth');
+const { User } = require('../../db/models');
 const router = express.Router();
+
+// Sign up
+router.post(
+  '',
+  asyncHandler(async (req, res) => {
+    const { email, password, username } = req.body;
+    const user = await User.signup({ email, username, password });
+
+    await setTokenCookie(res, user);
+
+    return res.json({
+      user,
+    });
+  }),
+);
 
 module.exports = router;
