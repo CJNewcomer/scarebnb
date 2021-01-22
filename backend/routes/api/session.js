b8775b78ad19a64971020e869d31ac513db87b1d
Courtney Newcomer <courtney.j.newcomer@gmail.com>
2021-01-21 22:57:04 -0600

    user login api route finished

---
+++ backend/routes/api/session.js
@@ -0,0 +1,32 @@
+const express = require('express')
+const asyncHandler = require('express-async-handler');
+
+const { setTokenCookie, restoreUser } = require('../../utils/auth');
+const { User } = require('../../db/models');
+const router = express.Router();
+
+// Log in
+router.post(
+  '/',
+  asyncHandler(async (req, res, next) => {
+    const { credential, password } = req.body;
+
+    const user = await User.login({ credential, password });
+
+    if (!user) {
+      const err = new Error('Login failed');
+      err.status = 401;
+      err.title = 'Login failed';
+      err.errors = ['The provided credentials were invalid.'];
+      return next(err);
+    }
+
+    await setTokenCookie(res, user);
+
+    return res.json({
+      user,
+    });
+  }),
+);
+
+module.exports = router;
