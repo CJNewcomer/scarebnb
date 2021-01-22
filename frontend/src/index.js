5dc3ef6c923004362b0462556149c110e108ed8c
Courtney Newcomer <courtney.j.newcomer@gmail.com>
2021-01-22 12:04:50 -0600

    completed initial setup of redux store

---
+++ frontend/src/index.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import './index.css';
+import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { BrowserRouter } from 'react-router-dom';
+import App from './App';
+
+import configureStore from './store';
+
+const store = configureStore();
+
+if (process.env.NODE_ENV !== 'production') {
+  window.store = store;
+}
+
+function Root() {
+  return (
+    <Provider store={store}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </Provider>
+  )
+}
+
+ReactDOM.render(
+  <React.StrictMode>
+    <Root />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
