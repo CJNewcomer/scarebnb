4878632831f75cb38ae94d280ef4ddf8ac5ffa2d
Courtney Newcomer <courtney.j.newcomer@gmail.com>
2021-01-22 10:14:08 -0600

    validated login request body

--- backend/routes/api/session.js
+++ backend/routes/api/session.js
@@ -1,9 +1,22 @@
 const express = require('express')
 const asyncHandler = require('express-async-handler');
+const { check } = require('express-validator');
+const { handleValidationErrors } = require('../../utils/validation');
 
 const { setTokenCookie, restoreUser } = require('../../utils/auth');
 const { User } = require('../../db/models');
 const router = express.Router();
+
+const validateLogin = [
+  check('credential')
+    .exists({ checkFalsy: true })
+    .notEmpty()
+    .withMessage('Please provide a valid email or username.'),
+  check('password')
+    .exists({ checkFalsy: true })
+    .withMessage('Please provide a password.'),
+  handleValidationErrors,
+];
 
 // Restore session user
 router.get(
@@ -22,6 +35,7 @@ router.get(
 // Log in
 router.post(
   '/',
+  validateLogin,
   asyncHandler(async (req, res, next) => {
     const { credential, password } = req.body;
 
