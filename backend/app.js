38f7c6fc72c792abf92e133cbea296653f9354d0
Courtney Newcomer <courtney.j.newcomer@gmail.com>
2021-01-21 22:09:08 -0600

    finished initial backend setup

---
+++ backend/app.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const morgan = require('morgan');
+const cors = require('cors');
+const csurf = require('csurf');
+const helmet = require('helmet');
+const cookieParser = require('cookie-parser');
+const routes = require('./routes');
+const { environment } = require('./config');
+const isProduction = environment === 'production';
+
+const app = express();
+
+app.use(morgan('dev'));
+app.use(cookieParser());
+app.use(express.json());
+
+if (!isProduction) {
+    app.use(cors());
+}
+app.use(helmet({contentSecurityPolicy: false}));
+app.use(
+    csurf({
+        cookie: {
+            secure: isProduction,
+            sameSite: isProduction && "Lax",
+            httpOnly: true,
+        },
+    })
+);
+app.use(routes);
+    
+    module.exports = app;
