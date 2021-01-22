a0e644c1a093c43923b7eb6b5f6fb0eb1bf2e60f
Courtney Newcomer <courtney.j.newcomer@gmail.com>
2021-01-21 22:17:26 -0600

    finished Phase 2 API routes

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,5 +1,8 @@
 const express = require('express');
 const router = express.Router();
+const apiRouter = require('./api');
+
+router.use('/api', apiRouter);
 
 router.get('/hello/world', function(req, res) {
   res.cookie('XSRF-TOKEN', req.csrfToken());
