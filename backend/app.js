96aaeff155b5bcde7ee52bd62a1cd7422a3c0179
Courtney Newcomer <courtney.j.newcomer@gmail.com>
2021-01-21 22:20:13 -0600

    the real phase 2-error handling completed

--- backend/app.js
+++ backend/app.js
@@ -28,5 +28,35 @@ app.use(
     })
 );
 app.use(routes);
-    
+
+app.use((_req, _res, next) => {
+  const err = new Error("The requested resource couldn't be found.");
+  err.title = "Resource Not Found";
+  err.errors = ["The requested resource couldn't be found."];
+  err.status = 404;
+  next(err);
+});
+
+const { ValidationError } = require('sequelize');
+
+app.use((err, _req, _res, next) => {
+  // check if error is a Sequelize error:
+  if (err instanceof ValidationError) {
+    err.errors = err.errors.map((e) => e.message);
+    err.title = 'Validation error';
+  }
+  next(err);
+});
+
+app.use((err, _req, res, _next) => {
+  res.status(err.status || 500);
+  console.error(err);
+  res.json({
+    title: err.title || 'Server Error',
+    message: err.message,
+    errors: err.errors,
+    stack: isProduction ? null : err.stack,
+  });
+});
+
     module.exports = app;
