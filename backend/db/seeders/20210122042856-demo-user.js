b2409715e250e2d3831991656afe8bc294449548
Courtney Newcomer <courtney.j.newcomer@gmail.com>
2021-01-21 22:49:06 -0600

    finished phase 3 user auth

---
+++ backend/db/seeders/20210122042856-demo-user.js
@@ -0,0 +1,38 @@
+'use strict';
+const faker = require('faker');
+const bcrypt = require('bcryptjs');
+
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    /*
+      Add altering commands here.
+      Return a promise to correctly handle asynchronicity.
+
+      Example:
+      */
+      return queryInterface.bulkInsert('Users', [
+        {
+          email: 'demo@user.io',
+          username: 'Demo-lition',
+          hashedPassword: bcrypt.hashSync('password'),
+        },
+        {
+          email: faker.internet.email(),
+          username: 'FakeUser1',
+          hashedPassword: bcrypt.hashSync(faker.internet.password()),
+        },
+        {
+          email: faker.internet.email(),
+          username: 'FakeUser2',
+          hashedPassword: bcrypt.hashSync(faker.internet.password()),
+        },
+      ], {});
+  },
+
+  down: (queryInterface, Sequelize) => {
+    const Op = Sequelize.Op;
+    return queryInterface.bulkDelete('Users', {
+      username: { [Op.in]: ['Demo-lition', 'FakeUser1', 'FakeUser2'] }
+    }, {});
+  }
+};
