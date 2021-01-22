f19c157356f19dfb21b7639c3feafa0dd4c5d059
Courtney Newcomer <courtney.j.newcomer@gmail.com>
2021-01-22 16:41:40 -0600

    finishing frontend component testing

--- backend/routes/api/index.js
+++ backend/routes/api/index.js
@@ -1,9 +1,49 @@
 const router = require('express').Router();
 const sessionRouter = require('./session.js');
 const usersRouter = require('./users.js');
+const asyncHandler = require('express-async-handler');
+const { setTokenCookie } = require('../../utils/auth.js');
+const { User } = require('../../db/models');
 
 router.use('/session', sessionRouter);
 
 router.use('/users', usersRouter);
+
+router.post("/test", function (req, res) {
+    res.json({requestBody: req.body });
+});
+
+// // GET /api/set-token-cookie
+// router.get('/set-token-cookie', asyncHandler(async (req, res) => {
+//   const user = await User.findOne({
+//       where: {
+//         username: 'Demo-lition'
+//       },
+//     })
+//   setTokenCookie(res, user);
+//   return res.json({ user });
+// }));
+
+// // GET /api/restore-user
+// const { restoreUser } = require('../../utils/auth.js');
+// router.get(
+//   '/restore-user',
+//   restoreUser,
+//   (req, res) => {
+//     return res.json(req.user);
+//   }
+// );
+
+// // GET /api/require-auth
+// const { requireAuth } = require('../../utils/auth.js');
+// router.get(
+//   '/require-auth',
+//   requireAuth,
+//   (req, res) => {
+//     return res.json(req.user);
+//   }
+// );
+
+
 
 module.exports = router;
