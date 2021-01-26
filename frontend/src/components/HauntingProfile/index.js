194e2e9c5bc5166db9a934334dc696f30e15343f
Courtney Newcomer <courtney.j.newcomer@gmail.com>
2021-01-26 12:17:12 -0600

    added css to haunting profile

--- frontend/src/components/HauntingProfile/index.js
+++ frontend/src/components/HauntingProfile/index.js
@@ -1,11 +1,12 @@
 
 
-function HauntingProfile({ imgPath, locationName, city, state, country, description, price}) {
+function HauntingProfile({ imgPath, locationName, address, city, state, country, price, description}) {
     return (
-        <div className='card'>
+        <div className='spot'>
             <img src={imgPath} alt=""/>
-            <div className='card-info'>
+            <div className='spot__info'>
                 <h2>{locationName}</h2>
+                <h3>{address}</h3>
                 <h3>{city}</h3>
                 <h3>{state}</h3>
                 <h3>{country}</h3>
