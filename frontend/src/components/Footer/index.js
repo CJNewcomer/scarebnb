1747f5c76165e62e5878846da9ac242728f0fb43
Courtney Newcomer <courtney.j.newcomer@gmail.com>
2021-01-27 22:12:37 -0600

    rendering footer and header on every page

--- frontend/src/components/Footer/index.js
+++ frontend/src/components/Footer/index.js
@@ -1,15 +1,15 @@
-import './footer.css';
 
 function Footer() {
     return (
         <div className='footer'>
             <div className='footer__developer'>
+                <p>Created by Courtney J. Newcomer {new Date().getFullYear()}</p>
                 <div className='footer_developer-container'>
                     <a className='github' href="https://github.com/CJNewcomer">
-                        <i class="fab fa-github-square"></i>
+                        <i className="fab fa-github-square"></i>
                     </a>
                     <a className='linkedin' href="https://http://linkedin.com/in/courtney-newcomer-gray-474b0923">
-                        <i class="fab fa-linkedin"></i>
+                        <i className="fab fa-linkedin"></i>
                     </a>
                 </div>
             </div>
