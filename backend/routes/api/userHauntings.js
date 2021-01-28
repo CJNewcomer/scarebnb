998eedad6567ed9ecd0c394660d93e56037889eb
Courtney Newcomer <courtney.j.newcomer@gmail.com>
2021-01-28 13:28:41 -0600

    added userHauntings route skeleton

---
+++ backend/routes/api/userHauntings.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const router = express.Router();
+
+const { userHaunting } = require('../../db/models');
+const asyncHandler = require('express-async-handler');
+
+
+
+module.exports = router;
